6baa599d5cb83dd35e27d7741c7cdc39d4c09cbe
Rafael Alessandro <noprafa@gmail.com>
2018-06-07 10:16:38 -0300

    Source code inserted (#1)

---
+++ src/plate.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+import sys
+from operator import itemgetter
+
+frame = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
+#frame = cv2.pyrMeanShiftFiltering(frame, 31, 91)
+if len(sys.argv) > 2:
+	qt_characters = int(sys.argv[2])
+else:
+	qt_characters = 10
+
+gray_frame = cv2.cvtColor( frame, cv2.COLOR_RGB2GRAY )
+median_noise_reduction = cv2.medianBlur(gray_frame, 3)
+
+kernel = np.ones((3,3),np.uint8)
+eroded_frame = cv2.erode(median_noise_reduction, kernel, iterations = 1)
+dilated_frame = cv2.dilate(median_noise_reduction, kernel, iterations = 1)
+
+new_frame = dilated_frame - eroded_frame
+  
+kernel2 = np.ones((1, 70), np.uint8)
+
+eroded_frame2 = cv2.erode(new_frame, kernel2, iterations = 1)
+new_frame =  new_frame - eroded_frame2
+
+h, w = new_frame.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+
+(thresh, im_bw) = cv2.threshold(new_frame, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+new_frame2 = im_bw.copy()
+
+cv2.floodFill(new_frame2, mask, (0,0), 255)
+
+new_frame2 = 255-new_frame2
+#new_frame2 = cv2.erode(new_frame2, kernel, iterations = 1)
+#new_frame2 = cv2.dilate(new_frame2, kernel, iterations = 1)
+
+_, contours,_ = cv2.findContours(new_frame2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+imgsvector = []
+for contour in contours:
+	area = cv2.contourArea(contour)
+	if area > 250:
+		#cv2.drawContours(frame,contour,-1,(0,0,255),3) #desenha o contorno. Linha comentada pois estou desenhando retangulos em baixo
+		
+		#rect = cv2.minAreaRect(contour) #pega o retangulo minimo
+		#box = cv2.boxPoints(rect) #pontos da box
+		#box = np.int0(box) #converte pra int0
+		#if abs(box[0][0]-box[2][0])>70: continue #se a largura for maior que 70
+		#cv2.drawContours(frame,[box],-1,(0,255,0),1) #desenha
+		x,y,w,h = cv2.boundingRect(contour) #boundingboxes
+		if w > (2*h):
+			continue
+		cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2) #desenha retangulo
+		crop_img = new_frame2[y:y+h, x:x+w] #corta o retangulo
+		numero = x+y #variavel pra controlar o nome dos caracteres segmentados
+		crop_imgTuple = (area, crop_img, numero)
+		imgsvector.append(crop_imgTuple)
+		
+imgsvector = sorted(imgsvector, key=itemgetter(0), reverse=True)
+imgsvector = imgsvector[0:qt_characters]
+imgsvector = sorted(imgsvector, key=itemgetter(2))
+
+for imgpos in imgsvector:
+	cv2.imwrite("output/" + str(imgpos[2]) + ".png", imgpos[1])
+
+	
+	
+#cv2.drawContours(frame,contours,-1,(0,0,255),6)
+#cv2.imshow('NEW_IMAGE', frame)
+cv2.imwrite("output/output.png", new_frame2)
+cv2.imwrite("output/output2.png", frame)
+cv2.waitKey(0)
